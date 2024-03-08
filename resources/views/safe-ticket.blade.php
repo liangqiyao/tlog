57510855a1e1b91443f6d95f7bfdf2a540cee5b0
liangqiyao <liangqiyao@huazhen2008.com>
2024-03-08 17:20:13 +0800

    20240308-safe-new

--- resources/views/safe-ticket.blade.php
+++ resources/views/safe-ticket.blade.php
@@ -80,14 +80,12 @@ function getLuckNumber(){
   var luckNumber = '';
   var date = new Date();
   const str = localStorage.getItem('luckNumber');
-  if(str != ""){
+  if(str != "" && str!= undefined){
     const obj = JSON.parse(str);
     if(date.getTime() < obj.exp){
       luckNumber = obj.val;
     }
   }
-
-
   return luckNumber;
 }
 
