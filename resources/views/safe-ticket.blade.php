1cf1d051b0aa668bf39cd126cbd85db74452dce3
liangqiyao <liangqiyao@huazhen2008.com>
2024-03-08 13:53:31 +0800

    20240308-safe-new

--- resources/views/safe-ticket.blade.php
+++ resources/views/safe-ticket.blade.php
@@ -3,7 +3,11 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>抽奖</title>
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/layui/2.9.7/css/layui.css">
 </head>
 <body>
