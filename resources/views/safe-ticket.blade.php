a5a62a0db14a5ca6b2c0360c814caca053a28a45
liangqiyao <liangqiyao@huazhen2008.com>
2024-03-08 13:56:06 +0800

    20240308-safe-new

--- resources/views/safe-ticket.blade.php
+++ resources/views/safe-ticket.blade.php
@@ -55,11 +55,16 @@ layui.use(['form','jquery'], function(){
   var form = layui.form;
   //各种基于事件的操作，下面会有进一步介绍
   var $ = layui.jquery;
+    $.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
 form.on('submit(*)', function(data){
   console.log(data.elem) //被执行事件的元素DOM对象，一般为button对象
   console.log(data.form) //被执行提交的form对象，一般在存在form标签时才会返回
   console.log(data.field) //当前容器的全部表单字段，名值对形式：{name: value}
-  
+
    $.ajax({
             type: 'POST',
             url: '/safeTickect',
